778def0555f9a76718a2f697130b9fb9a8e74902
Romanescu Veronica <103960855+VeroCodingProjects@users.noreply.github.com>
2022-12-20 22:13:38 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+const words = []; //the list will contain the added words
+
+function addWords() {
+    let userInput = document.getElementById("userInput").value;
+    words.push(userInput);
+}
+
+function searchInput(input) { // searching for all the words that start with the input
+    let reg = input;
+    return words.filter(function(term) {
+        if (term.startsWith(reg)) {
+            return term;
+        }
+    });
+}
+  
+function showResults(val) { // printing the result
+    let res = document.getElementById("result");
+    res.innerHTML = '';
+    let list = '';
+    let terms = searchInput(val);
+    if (terms.length == 0) { // if the word doesn't exist, then it should be added to the dictionary
+        list += "This word does not exist. Please, add it to the dictionary! :)"
+    }
+    for (i = 0; i < terms.length; ++i) {
+        list += '<li>' + terms[i] + '</li>';
+    }
+    res.innerHTML = '<ul>' + list + '</ul>';
+}
