5323705fff818f734dd6e8fe637097f51885d6f6
vero_romanescu <romanescuveronica278@gmail.com>
2023-01-20 21:54:45 +0200

    second review

--- main.js
+++ main.js
@@ -1,29 +1,41 @@
-const words = []; //the list will contain the added words
+const words = [];
 
-function addWords() {
-    let userInput = document.getElementById("userInput").value;
-    words.push(userInput);
+// recives the form's elements, adds the word to the dictionary and prints it on the page
+function addWords(formData) {
+    if (words.includes(formData.input.value)) {
+        formData.repetitive.innerHTML = "The word is in the dictionary. Make sure you search for it.";
+        formData.input.value = '';
+        return 0;
+    }
+    formData.repetitive.innerHTML = '';
+    formData.dictionary.innerHTML += formData.input.value + " ";
+    words.push(formData.input.value);
+    formData.input.value = '';
 }
 
-function searchInput(input) { // searching for all the words that start with the input
-    let reg = input;
+// gets the input of the user and returns the terms that start with the input
+function searchInput(input) { 
     return words.filter(function(term) {
-        if (term.startsWith(reg)) {
+        if (term.startsWith(input)) {
             return term;
         }
     });
 }
-  
-function showResults(val) { // printing the result
-    let res = document.getElementById("result");
-    res.innerHTML = '';
+
+// gets the terms that matches the input and prints them in a list
+function showResults(val) { 
+    document.getElementById("result").innerHTML = '';
     let list = '';
     let terms = searchInput(val);
-    if (terms.length == 0) { // if the word doesn't exist, then it should be added to the dictionary
+    if (terms.length == 0) { 
         list += "This word does not exist. Please, add it to the dictionary! :)"
+    }
+    if (val == '') {
+        list = '';
+        return 0;
     }
     for (i = 0; i < terms.length; ++i) {
         list += '<li>' + terms[i] + '</li>';
     }
-    res.innerHTML = '<ul>' + list + '</ul>';
+    document.getElementById("result").innerHTML = '<ul>' + list + '</ul>';
 }
