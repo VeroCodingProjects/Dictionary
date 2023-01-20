c40464b2904c0e1970ec67ea05b11a081e8e21c8
vero_romanescu <romanescuveronica278@gmail.com>
2023-01-20 15:10:35 +0200

    sent the datas via form

--- main.js
+++ main.js
@@ -1,29 +1,27 @@
-const words = []; //the list will contain the added words
+const words = []; 
 
-function addWords() {
-    let userInput = document.getElementById("user-input").value;
-    if (words.includes(userInput)) {
+function addWords(formData) {
+    if (words.includes(formData.input.value)) {
         return 0;
     } else {
-        words.push(userInput);
+        words.push(formData.input.value);
     }
 }
 
-function searchInput(input) { // searching for all the words that start with the input
-    let reg = input;
+function searchInput(input) {
     return words.filter(function(term) {
-        if (term.startsWith(reg)) {
+        if (term.startsWith(input)) {
             return term;
         }
     });
 }
   
-function showResults(val) { // printing the result
+function showResults(val) {
     let res = document.getElementById("result");
     res.innerHTML = '';
     let list = '';
     let terms = searchInput(val);
-    if (terms.length == 0) { // if the word doesn't exist, then it should be added to the dictionary
+    if (terms.length == 0) {
         list += "This word does not exist. Please, add it to the dictionary! :)"
     }
     for (i = 0; i < terms.length; ++i) {
